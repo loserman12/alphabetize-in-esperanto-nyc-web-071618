aa3af9ca42789ee14afa52a3000d003eb08b2119
Your Name <you@example.com>
2018-07-10 13:30:16 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,14 @@
+Esperanto_Alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+#upper case varible = constant.. constant for duration of program
+#can be accessed anywhere in class or module
+#code works fine if varible is inside method also
 def alphabetize(arr)
-  # code here
+arr.sort_by do |words|
+# arugment arr is sorted by each of its elements which =  #{words}
+  words.split("").collect do |letter|
+#{words} is split by (""), collected and assigned 2 #{letter}
+Esperanto_Alphabet.index(letter)
+#sort by the index of Esperanto_Alphabet using argument(letter)
+end
+end
 end
